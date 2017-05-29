9f40b7983addd4d2750f5e48be0e1a4270a4abc9
Eric Stroczynski <eric.stroczynski@coreos.com>
2017-05-28 22:04:31 -0700

    deleter/: impl eip, ngw, igw, eni deleter fn's
    ElasticIP, NAT Gateway, Internet Gateway, and Elastic Network Interface
    deleter functions

--- deleter/deleter.go
+++ deleter/deleter.go
@@ -183,6 +183,88 @@ func deleteEC2AMIsByIDs(cfg *DeleteConfig, ids *[]string) error {
 	return nil
 }
 
+func deleteEC2EIPAssocationsByIDs(cfg *DeleteConfig, ids *[]string) error {
+	if ids == nil {
+		return nil
+	}
+
+	svc := ec2.New(cfg.AWSSession)
+	fmtStr := "Disassociated EC2 ElasticIP"
+	if cfg.DryRun {
+		fmtStr = drStr + " " + fmtStr
+	}
+	var params *ec2.DisassociateAddressInput
+	for _, id := range *ids {
+		params = &ec2.DisassociateAddressInput{
+			AssociationId: aws.String(id),
+			DryRun:        aws.Bool(cfg.DryRun),
+		}
+
+		ctx := aws.BackgroundContext()
+		_, err := svc.DisassociateAddressWithContext(ctx, params)
+		if err != nil {
+			if cfg.IgnoreErrors {
+				fmt.Printf("{\"error\": \"%s\"}\n", err.Error())
+				continue
+			}
+			aerr, ok := err.(awserr.Error)
+			if ok {
+				aerrCode := aerr.Code()
+				if aerrCode == drCode {
+					fmt.Println(fmtStr, id)
+					continue
+				}
+			}
+			return err
+		}
+		fmt.Println(fmtStr, id)
+		// Prevent throttling
+		time.Sleep(time.Duration(500) * time.Millisecond)
+	}
+	return nil
+}
+
+func deleteEC2EIPAddresses(cfg *DeleteConfig, ids *[]string) error {
+	if ids == nil {
+		return nil
+	}
+	// Format of ids should be
+	svc := ec2.New(cfg.AWSSession)
+	fmtStr := "Released EC2 ElasticIP"
+	if cfg.DryRun {
+		fmtStr = drStr + " " + fmtStr
+	}
+	var params *ec2.ReleaseAddressInput
+	for _, id := range *ids {
+		params = &ec2.ReleaseAddressInput{
+			AllocationId: aws.String(id),
+			DryRun:       aws.Bool(cfg.DryRun),
+		}
+
+		ctx := aws.BackgroundContext()
+		_, err := svc.ReleaseAddressWithContext(ctx, params)
+		if err != nil {
+			if cfg.IgnoreErrors {
+				fmt.Printf("{\"error\": \"%s\"}\n", err.Error())
+				continue
+			}
+			aerr, ok := err.(awserr.Error)
+			if ok {
+				aerrCode := aerr.Code()
+				if aerrCode == drCode {
+					fmt.Println(fmtStr, id)
+					continue
+				}
+			}
+			return err
+		}
+		fmt.Println(fmtStr, id)
+		// Prevent throttling
+		time.Sleep(time.Duration(500) * time.Millisecond)
+	}
+	return nil
+}
+
 func deleteEC2InstancesByIDs(cfg *DeleteConfig, ids *[]string) error {
 	if ids == nil {
 		return nil
@@ -238,6 +320,261 @@ func deleteEC2InstancesByIDs(cfg *DeleteConfig, ids *[]string) error {
 	}
 	// Instances take awhile to shut down
 	time.Sleep(time.Duration(2) * time.Minute)
+	return nil
+}
+
+func deleteEC2InternetGatewayAttachments(cfg *DeleteConfig, igws *[]*ec2.InternetGateway) error {
+	if igws == nil {
+		return nil
+	}
+
+	svc := ec2.New(cfg.AWSSession)
+	fmtStr := "Detached EC2 InternetGateway"
+	if cfg.DryRun {
+		fmtStr = drStr + " " + fmtStr
+	}
+
+	var params *ec2.DetachInternetGatewayInput
+	for _, igw := range *igws {
+		params = &ec2.DetachInternetGatewayInput{
+			InternetGatewayId: igw.InternetGatewayId,
+			DryRun:            aws.Bool(cfg.DryRun),
+		}
+
+		for _, a := range igw.Attachments {
+			params.SetVpcId(*a.VpcId)
+			ctx := aws.BackgroundContext()
+			_, err := svc.DetachInternetGatewayWithContext(ctx, params)
+			if err != nil {
+				if cfg.IgnoreErrors {
+					fmt.Printf("{\"error\": \"%s\"}\n", err.Error())
+					continue
+				}
+				aerr, ok := err.(awserr.Error)
+				if ok {
+					aerrCode := aerr.Code()
+					if aerrCode == drCode {
+						fmt.Println(fmtStr, *igw.InternetGatewayId, "from", *a.VpcId)
+						continue
+					}
+				}
+				return err
+			}
+			fmt.Printf("%s %s from VPC %s\n", fmtStr, *igw.InternetGatewayId, *a.VpcId)
+			// Prevent throttling
+			time.Sleep(time.Duration(500) * time.Millisecond)
+		}
+	}
+	return nil
+}
+
+// NOTE: must detach all internet gateways from vpc's before deletion
+func deleteEC2InternetGatewaysByIDs(cfg *DeleteConfig, ids *[]string) error {
+	if ids == nil {
+		return nil
+	}
+
+	igws, ierr := describe.GetEC2InternetGatewaysByIDs(ids)
+	if ierr != nil {
+		return nil
+	}
+
+	// Detach internet gateways from all vpc's
+	if verr := deleteEC2InternetGatewayAttachments(cfg, igws); verr != nil {
+		return verr
+	}
+
+	svc := ec2.New(cfg.AWSSession)
+	fmtStr := "Deleted EC2 InternetGateway"
+	if cfg.DryRun {
+		fmtStr = drStr + " " + fmtStr
+	}
+	var params *ec2.DeleteInternetGatewayInput
+	for _, id := range *ids {
+		params = &ec2.DeleteInternetGatewayInput{
+			InternetGatewayId: aws.String(id),
+			DryRun:            aws.Bool(cfg.DryRun),
+		}
+
+		ctx := aws.BackgroundContext()
+		_, err := svc.DeleteInternetGatewayWithContext(ctx, params)
+		if err != nil {
+			if cfg.IgnoreErrors {
+				fmt.Printf("{\"error\": \"%s\"}\n", err.Error())
+				continue
+			}
+			aerr, ok := err.(awserr.Error)
+			if ok {
+				aerrCode := aerr.Code()
+				if aerrCode == drCode {
+					fmt.Println(fmtStr, id)
+					continue
+				}
+			}
+			return err
+		}
+		fmt.Println(fmtStr, id)
+		// Prevent throttling
+		time.Sleep(time.Duration(500) * time.Millisecond)
+	}
+	return nil
+}
+
+func deleteEC2NatGatewaysByIDs(cfg *DeleteConfig, ids *[]string) error {
+	if ids == nil {
+		return nil
+	}
+
+	svc := ec2.New(cfg.AWSSession)
+	fmtStr := "Deleted EC2 NatGateway"
+	if cfg.DryRun {
+		for _, id := range *ids {
+			fmt.Println(drStr, fmtStr, id)
+		}
+		return nil
+	}
+	var params *ec2.DeleteNatGatewayInput
+	for _, id := range *ids {
+		params = &ec2.DeleteNatGatewayInput{
+			NatGatewayId: aws.String(id),
+		}
+
+		ctx := aws.BackgroundContext()
+		_, err := svc.DeleteNatGatewayWithContext(ctx, params)
+		if err != nil {
+			if cfg.IgnoreErrors {
+				fmt.Printf("{\"error\": \"%s\"}\n", err.Error())
+				continue
+			}
+			return err
+		}
+		fmt.Println(fmtStr, id)
+		// Prevent throttling
+		time.Sleep(time.Duration(500) * time.Millisecond)
+	}
+	// Wait for NAT Gateways to delete
+	time.Sleep(time.Duration(30) * time.Second)
+	return nil
+}
+
+// Network interfaces require detachment before deletion
+func detachEC2NetworkInterfacesByIDs(cfg *DeleteConfig, ids *[]string) error {
+	if ids == nil {
+		return nil
+	}
+
+	svc := ec2.New(cfg.AWSSession)
+	fmtStr := "Detached EC2 NetworkInterface"
+	if cfg.DryRun {
+		fmtStr = drStr + " " + fmtStr
+	}
+	var params *ec2.DetachNetworkInterfaceInput
+
+	for _, id := range *ids {
+		params = &ec2.DetachNetworkInterfaceInput{
+			AttachmentId: aws.String(id),
+			Force:        aws.Bool(true),
+			DryRun:       aws.Bool(cfg.DryRun),
+		}
+
+		ctx := aws.BackgroundContext()
+		_, err := svc.DetachNetworkInterfaceWithContext(ctx, params)
+		if err != nil {
+			if cfg.IgnoreErrors {
+				fmt.Printf("{\"error\": \"%s\"}\n", err.Error())
+				continue
+			}
+			aerr, ok := err.(awserr.Error)
+			if ok {
+				aerrCode := aerr.Code()
+				if aerrCode == drCode {
+					fmt.Println(fmtStr, id)
+					continue
+				}
+			}
+			return err
+		}
+		fmt.Println(fmtStr, id)
+		// Prevent throttling
+		time.Sleep(time.Duration(500) * time.Millisecond)
+	}
+	return nil
+}
+
+func deleteEC2NetworkInterfacesByIDs(cfg *DeleteConfig, ids *[]string) error {
+	if ids == nil {
+		return nil
+	}
+
+	// To delete a network interface, all attachments must be deleted first.
+	// DescribeNetworkInterfaces will find attachment ID's.
+	enis, nierr := describe.GetEC2NetworkInterfacesByIDs(ids)
+	if nierr != nil {
+		return nierr
+	}
+
+	if enis != nil {
+		eniaIDs := make([]string, 0)
+		iIDs := make([]string, 0)
+		for _, ni := range *enis {
+			if ni.Attachment != nil && ni.Attachment.AttachmentId != nil {
+				// eth0 is the primary eni and cannot be detached. Must delete underlying
+				// instance
+				if ni.Attachment.DeviceIndex != nil && *ni.Attachment.DeviceIndex == 0 {
+					iIDs = append(iIDs, *ni.Attachment.InstanceId)
+					continue
+				}
+				eniaIDs = append(eniaIDs, *ni.Attachment.AttachmentId)
+			}
+		}
+		// Remove blocking instances
+		_ = deleteEC2InstancesByIDs(cfg, &iIDs)
+
+		if eniaIDs != nil {
+			if nerr := detachEC2NetworkInterfacesByIDs(cfg, &eniaIDs); nerr != nil {
+				return nerr
+			}
+		}
+	}
+
+	// Format of ids should be
+	svc := ec2.New(cfg.AWSSession)
+	fmtStr := "Deleted EC2 NetworkInterface"
+	if cfg.DryRun {
+		fmtStr = drStr + " " + fmtStr
+	}
+	var params *ec2.DeleteNetworkInterfaceInput
+
+	for _, id := range *ids {
+		if id == "" {
+			continue
+		}
+		params = &ec2.DeleteNetworkInterfaceInput{
+			NetworkInterfaceId: aws.String(id),
+			DryRun:             aws.Bool(cfg.DryRun),
+		}
+
+		ctx := aws.BackgroundContext()
+		_, err := svc.DeleteNetworkInterfaceWithContext(ctx, params)
+		if err != nil {
+			if cfg.IgnoreErrors {
+				fmt.Printf("{\"error\": \"%s\"}\n", err.Error())
+				continue
+			}
+			aerr, ok := err.(awserr.Error)
+			if ok {
+				aerrCode := aerr.Code()
+				if aerrCode == drCode {
+					fmt.Println(fmtStr, id)
+					continue
+				}
+			}
+			return err
+		}
+		fmt.Println(fmtStr, id)
+		// Prevent throttling
+		time.Sleep(time.Duration(500) * time.Millisecond)
+	}
 	return nil
 }
 
@@ -449,13 +786,18 @@ func DeleteAWSResourcesByIDs(cfg *DeleteConfig, ids *[]string) error {
 		return deleteEC2AMIsByIDs(cfg, ids)
 	case arn.EC2CustomerGatewayRType:
 	case arn.EC2EIPRType:
+		return deleteEC2EIPAddresses(cfg, ids)
 	case arn.EC2EIPAssociationRType:
+		return deleteEC2EIPAssocationsByIDs(cfg, ids)
 	case arn.EC2InstanceRType:
 		return deleteEC2InstancesByIDs(cfg, ids)
 	case arn.EC2InternetGatewayRType:
+		return deleteEC2InternetGatewaysByIDs(cfg, ids)
 	case arn.EC2NatGatewayRType:
+		return deleteEC2NatGatewaysByIDs(cfg, ids)
 	case arn.EC2NetworkACLRType:
 	case arn.EC2NetworkInterfaceRType:
+		return deleteEC2NetworkInterfacesByIDs(cfg, ids)
 	case arn.EC2RouteTableAssociationRType:
 	case arn.EC2RouteTableRType:
 	case arn.EC2SecurityGroupRType:
