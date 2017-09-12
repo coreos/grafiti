53307f33ce35b1587e525d10556c781bd107c5fb
cpanato <ctadeu@gmail.com>
2017-09-12 12:59:24 +0200

    test/ci: fix dsl

--- jenkins-jobs/grafiti_multibranch_job.groovy
+++ jenkins-jobs/grafiti_multibranch_job.groovy
@@ -3,31 +3,31 @@
 multibranchPipelineJob("grafiti") {
   description("Tag and remove AWS Resources with Automation.\nThis job is managed by grafiti git repository.\nChanges here will be reverted automatically.")
   branchSources {
-    branchSource {
-      source {
-        github {
-          scanCredentialsId("37477e0c-2ab6-46fe-a83b-64b1add4777d")
-          checkoutCredentialsId("37477e0c-2ab6-46fe-a83b-64b1add4777d")
-          apiUri("")
-          repoOwner("coreos")
-          repository("grafiti")
-          buildForkPRHead(false)
-          buildForkPRMerge(true)
-          buildOriginBranch(true)
-          buildOriginBranchWithPR(false)
-          buildOriginPRHead(false)
-          buildOriginPRMerge(true)
-        }
-      }
-      strategy {
-        defaultBranchPropertyStrategy {
-          props {
-            noTriggerBranchProperty()
-          }
+    github {
+      scanCredentialsId("37477e0c-2ab6-46fe-a83b-64b1add4777d")
+      checkoutCredentialsId("37477e0c-2ab6-46fe-a83b-64b1add4777d")
+      apiUri("")
+      repoOwner("coreos")
+      repository("grafiti")
+      buildForkPRHead(false)
+      buildForkPRMerge(true)
+      buildOriginBranch(true)
+      buildOriginBranchWithPR(false)
+      buildOriginPRHead(false)
+      buildOriginPRMerge(true)
+    }
+  }
+
+  configure { project ->
+    project / 'sources' / 'data' / 'jenkins.branch.BranchSource'/ strategy(class: 'jenkins.branch.DefaultBranchPropertyStrategy') {
+      properties(class: 'java.util.Arrays$ArrayList') {
+        a(class: 'jenkins.branch.BranchProperty-array'){
+          'jenkins.branch.NoTriggerBranchProperty'()
         }
       }
     }
   }
+
   orphanedItemStrategy {
     discardOldItems {
       numToKeep(50)
